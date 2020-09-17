12b2db40ae9db62b404567d15888bb6893ceae7d
Karl Hoech <karl.hoech@collins.com>
2020-09-17 06:08:01 -0500

    Add removals

---
+++ s3tools/RemoveChildFromCompositeRepository.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3 
+# encoding: utf-8
+'''
+s3tools.RemoveChildFromP2CompositeRepository -- shortdesc
+
+s3tools.RemoveChildFromP2CompositeRepository is a description
+
+It defines classes_and_methods
+
+@copyright:  2020 Collins Aerospace. All rights reserved.
+
+@license:    BSD 3-Clause License
+'''
+
+import boto3
+import logging
+import os
+import pprint
+import sys
+import xml.etree.ElementTree as ElementTree
+
+from argparse import ArgumentParser
+from argparse import RawDescriptionHelpFormatter
+from gi.importer import repository
+from posixpath import join as urljoin
+
+import P2CompositeUtils
+import S3Utils
+
+__all__ = []
+__version__ = 0.1
+__date__ = '2020-07-21'
+__updated__ = '2020-07-21'
+
+DEBUG = 1
+TESTRUN = 0
+PROFILE = 0
+
+DEFAULT_SITE = '''http://ca-trustedsystems-dev-us-east-1.s3-website-us-east-1.amazonaws.com/'''
+DEFAULT_BUCKET_NAME = '''ca-trustedsystems-dev-us-east-1'''
+DEFAULT_BUCKET_PREFIX = '''p2'''
+
+
+def main(argv=None): # IGNORE:C0111
+    '''Command line options.'''
+
+    if argv is None:
+        argv = sys.argv
+    else:
+        sys.argv.extend(argv)
+
+    program_name = os.path.basename(sys.argv[0])
+    program_version = "v%s" % __version__
+    program_build_date = str(__updated__)
+    program_version_message = '%%(prog)s %s (%s)' % (program_version, program_build_date)
+    program_shortdesc = __import__('__main__').__doc__.split("\n")[1]
+    program_license = '''%s
+
+  Copyright 2020 Collins Aerospace. All rights reserved.
+
+  Licensed under the BSD 3-Clause License
+  http://raw.githubusercontent.com/loonwerks/s3-tools/master/LICENSE
+
+  Distributed on an "AS IS" basis without warranties
+  or conditions of any kind, either express or implied.
+
+USAGE
+''' % (program_shortdesc)
+
+    try:
+        # Setup argument parser
+        parser = ArgumentParser(description=program_license, formatter_class=RawDescriptionHelpFormatter)
+        parser.add_argument("-v", "--verbose",
+            dest="verbose", action="count", help="set verbosity level [default: %(default)s]")
+        parser.add_argument('-V', '--version',
+            action='version', version=program_version_message)
+        parser.add_argument('--bucket', dest="bucket",
+            help="the AWS bucket in which the P2 composite repositories are stored [default: %(default)]",
+            metavar="bucket",
+            default=DEFAULT_BUCKET_NAME)
+        parser.add_argument('--prefix', dest="prefix",
+            help="the key prefix in the AWS bucket from which to remove the child repositories [default: %(default)]",
+            metavar="prefix",
+            default=DEFAULT_BUCKET_PREFIX)
+        parser.add_argument('--child-name', dest="child_name",
+            action='append',
+            help="child names for each path [default: is the last segment of the corresponding path]",
+            metavar="child_name",
+            nargs='+')
+
+        # Process arguments
+        args = parser.parse_args()
+
+        bucket_name = args.bucket
+        bucket_prefix = args.prefix
+        child_names = [item for sublist in ([] if args.child_name is None else args.child_name) for item in sublist]
+
+        verbose = args.verbose
+
+        if verbose > 0:
+            print("Verbose mode on")
+
+        pprint.pprint(child_names)
+
+        cred = boto3.session.Session().get_credentials()
+        if cred is None:
+            print('Please provide Boto3 credentials.')
+            sys.exit(-1)
+
+        for child_name in child_names:
+            print('remove_repository_from_composite(%s, %s, %s)' % (bucket_name, bucket_prefix, child_name))
+            P2CompositeUtils.remove_repository_from_composite(bucket_name, bucket_prefix, child_name)
+
+        return 0
+    except KeyboardInterrupt:
+        ### handle keyboard interrupt ###
+        return 0
+    except Exception as e:
+        if DEBUG or TESTRUN:
+            raise(e)
+        indent = len(program_name) * " "
+        sys.stderr.write(program_name + ": " + repr(e) + "\n")
+        sys.stderr.write(indent + "  for help use --help")
+        return 2
+
+if __name__ == "__main__":
+    if DEBUG:
+        sys.argv.append("-v")
+    if TESTRUN:
+        import doctest
+        doctest.testmod()
+    if PROFILE:
+        import cProfile
+        import pstats
+        profile_filename = 's3tools.DeployToP2CompositeRepository_profile.txt'
+        cProfile.run('main()', profile_filename)
+        statsfile = open("profile_stats.txt", "wb")
+        p = pstats.Stats(profile_filename, stream=statsfile)
+        stats = p.strip_dirs().sort_stats('cumulative')
+        stats.print_stats()
+        statsfile.close()
+        sys.exit(0)
+    sys.exit(main())
